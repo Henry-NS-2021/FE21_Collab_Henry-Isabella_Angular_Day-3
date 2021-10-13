7fb2219b68a728405ba70493452c7e3e40b187b3
Henry <h.ngosytchev@gmail.com>
2021-10-13 15:46:57 +0200

    individual display for each product created

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { products } from '../products';
 
 @Component({
   selector: 'product-details',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+  product : any;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      let id : any = params.get('Id');
+      this.product = products[id];
+    });
   }
 
 }
