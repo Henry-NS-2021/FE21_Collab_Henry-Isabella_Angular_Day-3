bd4116e83730afe4f10f22954eedbbc12ac6b95a
Isabella <isabella.hartwig@gmail.com>
2021-10-13 14:13:32 +0200

    products get displayed correctly

--- src/app/products.ts
+++ src/app/products.ts
@@ -3,42 +3,42 @@ export const products =[
         name: "Scratch-me-hard Cat Tree",
         color:"light",
         material:"wood, fake-fur, rope, plastic",
-        image:"catTree4.jpg",
+        image:"assets/img/catTree4.jpg",
         price: 120
 
     },{
         name:"Rub-your-side Cat Tree",
         color:"gray",
         material:"wood, fake-fur, rope, plastic",
-        image:"catTree3.jpg",
+        image:"assets/img/catTree3.jpg",
         price:130
 
     },{
         name:"Cuddle-Buddy",
         color:"light",
         material:"wood, fake-fur, rope, plastic",
-        image:"catTree1.jpg",
+        image:"assets/img/catTree1.jpg",
         price:125
 
     },{
         name:"Fluffy House",
         color:"dark",
         material:"wood, fake-fur, rope, plastic",
-        image:"catTree5.jpg",
+        image:"assets/img/catTree5.jpg",
         price:150
 
     },{
         name:"Lion's Cave",
         color:"dark",
         material:"wood, fake-fur, rope, plastic",
-        image: "catTree2.jpg",
+        image: "assets/img/catTree2.jpg",
         price: 100
 
     },{
         name:"Be-a-Tiger",
         color:"orange",
         material:"wood, fake-fur, rope, plastic",
-        image:"catTree6.jpg",
+        image:"assets/img/catTree6.jpg",
         price: 55
 
     },
